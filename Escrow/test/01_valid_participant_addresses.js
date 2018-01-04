bb8d5ac288c3537640d10049424665c65d9e3354
Stu Peters <stu_peters187@hotmail.com>
2018-01-04 16:18:47 -0500

    More refactoring

--- Escrow/test/01_valid_participant_addresses.js
+++ Escrow/test/01_valid_participant_addresses.js
@@ -3,30 +3,24 @@ var Escrow = artifacts.require("./Escrow.sol");
 contract('Escrow', function(accounts){
 
   it("should have a buyer address that is valid", () => {
-    let myEscrowInstance;
     return Escrow.deployed().then(instance => {
-      myEscrowInstance = instance;
-      return myEscrowInstance.buyer()
+      return instance.buyer()
     }).then(buyer => {
       assert.notEqual(buyer, "0x0", "Buyer does not have a valid address");
     });
   });
 
   it("should have a seller address that is valid", () => {
-    let myEscrowInstance;
     return Escrow.deployed().then(instance => {
-      myEscrowInstance = instance;
-      return myEscrowInstance.seller()
+      return instance.seller()
     }).then(seller => {
       assert.notEqual(seller, "0x0", "Seller does not have a valid address");
     });
   });
 
   it("should have an arbiter address that is valid", () => {
-    let myEscrowInstance;
     return Escrow.deployed().then(instance => {
-      myEscrowInstance = instance;
-      return myEscrowInstance.arbiter()
+      return instance.arbiter()
     }).then(arbiter => {
       assert.notEqual(arbiter, "0x0", "Arbiter does not have a valid address");
     });
--- Escrow/test/03_sendToSeller_function.js
+++ Escrow/test/03_sendToSeller_function.js
@@ -3,6 +3,6 @@ var Escrow = artifacts.require("./Escrow.sol");
 contract('Escrow', accounts => {
 
   it("the buyer is able to send the funds to the seller", () => {
-
+    
   });
 })
